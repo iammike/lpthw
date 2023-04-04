1111fbfaa402a1574072c9be389587e46af994d5
Michael Collins <collins.m@slack-corp.com>
2023-04-04 09:22:44 -0400

    Day 1

---
+++ ex2.py
@@ -0,0 +1,9 @@
+# A comment
+# Anything after the # is ignored
+
+print("I could have code like this") #and the comment after is ignored
+
+# You can also use it to disable a line
+# print("This won't run")
+
+print("This will run.")
